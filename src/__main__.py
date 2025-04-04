db8f23e5fea19c20424413f0d43020cd12fa5c87
pgxtips <brandongill123@gmail.com>
2025-04-04 11:00:08 +0100

    feat: project restructure

---
+++ src/__main__.py
@@ -0,0 +1,5 @@
+import src.server as s
+
+if __name__ == '__main__':
+    s.start_server()
+
--- src/server/__init__.py
+++ src/server/__init__.py
@@ -1,6 +1,5 @@
-from flask import Flask
 from .globals import app_server
 from .router import * 
 
-if __name__ == "__main__":
+def start_server():
     app_server.run(host="0.0.0.0", port=6969, debug=True)
