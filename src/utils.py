647f9828b97483515f8267a03f05f5c3be38a1e8
pgxtips <brandongill123@gmail.com>
2025-04-19 10:02:12 +0100

    fix: forgot to add utils file

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+def flatten_dict(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
