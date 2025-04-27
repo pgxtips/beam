0dc653a694bf32d06d6947d9b5be04d0d3a88fd3
pgxtips <brandongill123@gmail.com>
2025-04-27 09:40:00 +0100

    feat: settings redeeming complete

--- static/js/pages/settings/settingsInit.js
+++ static/js/pages/settings/settingsInit.js
@@ -1,3 +1,9 @@
 $(document).ready(async function(){
+
+    $("select, input").change(function(event){
+        console.log("input changed")
+        $("#saveButtons").show()
+    })
+
     await settingsRedeemer()
 })
--- static/js/pages/settings/settingsRedeemer.js
+++ static/js/pages/settings/settingsRedeemer.js
@@ -1,13 +1,21 @@
 async function settingsRedeemer(){
-    await get_controller("/internal/settings", cbSettings)
+    await get_controller("/internal/settings", cbSettings, {})
 }
 
-function cbSettings(data){
+function cbSettings(props, data){
     let model_type = data.model_type ?? "N/A"
     let batch_size = data.batch_size ?? "N/A"
     let auto_training = data.auto_training ?? "N/A"
     let training_interval = data.training_interval ?? "N/A"
     let ui_theme = data.ui_theme ?? "N/A"
 
+    $("#selectModelInput").val(model_type)
+    $("#batchSizeInput").val(batch_size)
+    $("#selectAutoTrainingInput").val(auto_training)
+    $("#selectIntervalInput").val(training_interval)
+    $("selectUIThemeInput").val(ui_theme)
+    
+
+    $(".loading-state").hide()
     console.log(data)
 }
